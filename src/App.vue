3c1b8a4faaf0f6ea8676da159eb83f4ba0fad755
Mark van Lit <mark@livewallgroup.com>
2021-01-20 21:03:47 +0100

    Les 01

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="nav">
+      <div class="title">Vue cursus</div>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+.nav {
+  background-color: cadetblue;
+  height: 3rem;
+  .title {
+    padding: 1rem;
+    text-transform: uppercase;
+    font-weight: bold;
+    color: white;
+    display: inline-block;
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <p>Welkom {{naam}} bij de cursus.</p>
+
+    <div class="deelnemer" v-for="deelnemer in deelnemers" :key="deelnemer.id">
+      <div class="naam">{{deelnemer.naam}}</div>
+      <div class="afdeling">{{deelnemer.afdeling}}</div>
+    </div>
+
+    <br>
+    <h2>Voeg nieuwe deelnemer toe</h2>
+
+    <div>Naam:</div>
+    <input type="text" v-model="nieuweDeelnemerNaam">
+
+    <div>Afdeling:</div>
+    <input type="text" v-model="nieuweDeelnemerAfdeling">
+
+    <br>
+    <br>
+    <button @click="voegNieuweDeelnemerToe">Voeg toe</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      naam: "Piet",
+      nieuweDeelnemerNaam: "",
+      nieuweDeelnemerAfdeling: "",
+      deelnemers: [
+        {
+          id: 1,
+          naam: "Mark van Lit",
+          afdeling: "Platforms"
+        },
+        {
+          id: 2,
+          naam: "Pim Willems",
+          afdeling: "Platforms"
+        },
+        {
+          id: 3,
+          naam: "Tom Luijten",
+          afdeling: "Platforms"
+        },
+        {
+          id: 4,
+          naam: "Jeffrey Happel",
+          afdeling: "Engagement"
+        },
+        {
+          id: 5,
+          naam: "Nick Goossens",
+          afdeling: "Engagement"
+        },
+        {
+          id: 6,
+          naam: "Dennis Leenders",
+          afdeling: "Engagement"
+        }
+      ]
+    };
+  },
+  methods: {
+    voegNieuweDeelnemerToe() {
+      if (this.nieuweDeelnemerNaam && this.nieuweDeelnemerAfdeling) {
+        this.deelnemers.push({
+          id: 10,
+          naam: this.nieuweDeelnemerNaam,
+          afdeling: this.nieuweDeelnemerAfdeling
+        });
+        this.nieuweDeelnemerNaam = "";
+        this.nieuweDeelnemerAfdeling = "";
+      } else {
+        alert("Vul beide velden in");
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.deelnemer {
+  padding: 1rem;
+  background-color: cornflowerblue;
+  margin: 10px;
+  .naam {
+    font-size: 2rem;
+    color: white;
+    font-weight: bold;
+  }
+  .afdeling {
+    margin-left: 10px;
+  }
+}
+</style>
